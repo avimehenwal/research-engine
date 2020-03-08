75ca8264c7c9a0b3e9cc81f677c0f9e209cd70c5
avimehenwal <avi.mehanwal@gmail.com>
2020-03-08 19:22:37 +0100

    Appbar with search box

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,23 @@
 <template>
   <v-app>
+
+    <v-app-bar color="white" app dense flat short>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Search App</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-form>
+        <v-container class="mt-10">
+          <v-row >
+            <v-text-field outlined label="Search"></v-text-field>
+          </v-row>
+        </v-container>
+      </v-form>
+
+      <v-icon>mdi-magnify</v-icon>
+      <v-btn icon> <v-icon>mdi-dots-vertical</v-icon> </v-btn>
+    </v-app-bar>
+
     <v-content>
       <router-view></router-view>
     </v-content>
